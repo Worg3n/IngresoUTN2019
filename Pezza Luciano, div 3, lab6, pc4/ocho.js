68b94983c95b1c1b069ae209525dadf9ac70ce49
Worg3n <47321345+Worg3n@users.noreply.github.com>
2019-03-09 20:22:48 -0300

    Add files via upload

--- Pezza Luciano, div 3, lab6, pc4/ocho.js
+++ Pezza Luciano, div 3, lab6, pc4/ocho.js
@@ -1,67 +1,61 @@
 function mostrar() {
-    var letras;
-    var numeros;
-    var suma = 0;
-    var contpares = 0;
-    var cant0 = 0;
-    var prompos;
-    var sumaneg = 0;
-    var max;
-    var min;
-    var respuesta;
-    var cantimpares = 0;
-    var contpos = 0;
-    var flag = 0;
-    do {
+  var numero;
+  var respuesta;
+  var acumuladorpos = 0;
+  var contpos = 0;
+  var contpares = 0;
+  var contimpares = 0;
+  var cont0 = 0;
+  var promedio;
+  var sumaneg = 0;
+  var max;
+  var min;
+  var letra;
+  var flag = 0;
+  var sumapos = 0;
+  var letramax;
+  var letramin;
+  do {
+      numero = parseInt(prompt("Ingrese un numero que prefiera"));
+      while (numero < -100 || numero > 100) {
+          numero = parseInt(prompt("Error. Reingrese un numero valido"));
+      }
+      letra = prompt("Ingrere la letra que quiera");
+      while (!(isNaN(letra))) {
+          letra = prompt("Error. Reingrere una letra valida");
+      }
+      respuesta = confirm("Quiere repetir el proceso?");
 
-        numeros = parseInt(prompt("Introduzca un numero las veces que quiera"));
+      if (numero > 0) {
+          contpos++;
+          acumuladorpos += numero;
+      }
+      else if (numero < 0) {
+          sumaneg += numero;
+      }
+      else {
+          cont0++;
+      }
 
-        while (numeros < -100 || numeros > 100) {
-            numeros = parseInt(prompt("Error. Reintroduzca un numero las veces que quiera"));
-        }
-        letras = prompt("Introduzca una letra");
-
-        while (!(isNaN(letras))) {
-            letras = prompt("Introduzca una letra valida");
-        }
-
-        if (numeros >= 1) {
-            contpos++;
-
-        }
-        else if (numeros <= -1) {
-            sumaneg = sumaneg + numeros;
-        }
-        else {
-            cant0++;
-        }
-        if (numeros % 2 == 0) {
-            contpares++;
-        }
-        else {
-            cantimpares++;
-        }
+      if (numero % 2 == 0) {
+          contpares++;
+      } else {
+          contimpares++;
+      }
+      if (flag == 0 || numero > max) {
+          max = numero;
+          letramax = letra;
 
 
-        if (numeros < max || flag == 0) {
-            max = numeros;
-            letras = numeros;
-        }
+      } if (flag == 0 || numero < min) {
+          min = numero;
+          letramin = letra;
+          flag = 1;
+      }
 
-        if (numeros > min || flag == 0) {
-            min = numeros;
-            letras = numeros;
-            flag = 1;
+  }
+  while (respuesta);
+  promedio = acumuladorpos / contpos;
 
-        }
-
-        respuesta = prompt("Introduzca s para repetir el proceso");
-        suma = suma + numeros;
-    }
-    while (respuesta == "s");
-    prompos = suma / contpos;
-
-    document.write("el la cantidad de pares que hay son " + contpares + "<br>" + "La cantidad de numeros imapres son " + cantimpares + "<br>" + "La cantidad de 0 son " + cant0 + "<br>" + "El promedio de todos los positvios ingresados son:" + prompos + "<br>" + "La suma de los negativos es: " + sumaneg + "<br>" + "El numero y letra del max y el minimo son:" + min + "" + letras + " y " + max + "" + letras);
-
+  document.write("La cantidad de pares es: " + contpares + "<br> La cantidad de impares es: " + contimpares + "<br> La cantidad de ceros es: " + cont0 + "<br> El promedio de todos los números positivos ingresados: " + promedio + "<br> La suma de todos los números negativos: " + sumaneg + "<br> El número y la letra del máximo y el mínimo: " + max + " " + letramax + " " + min + " " + letramin);
 }
-
